efdf8cc6b0732d2f486a838a503169f0d00d55d9
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-04-15 02:37:56 -0500

    Se crea el patron singleton para la clase Server

--- clases/server.ts
+++ clases/server.ts
@@ -5,20 +5,24 @@ import http from 'http';
 
 export default class Server {
 
-
+    private static _intance:Server;
     public app:express.Application;
     public port:number;
     public io: socketIO.Server;
     private httpServer:http.Server;
 
 
-    constructor(){
+    private constructor(){
         this.app=express();
         this.port=SERVER_PORT;
         this.httpServer=new http.Server(this.app);
         this.io=new socketIO.Server(this.httpServer);
         this.escuharSockets();
 
+    }
+
+    public static get instance(){
+      return this._intance || (this._intance=new this());
     }
 
 
