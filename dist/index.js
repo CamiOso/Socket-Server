fb650f46fce274f872ac3d68aa68c816aca35d69
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-04-15 01:48:16 -0500

    Se realiza la configuracion del cors

--- dist/index.js
+++ dist/index.js
@@ -7,10 +7,14 @@ const server_1 = __importDefault(require("./clases/server"));
 const environment_1 = require("./global/environment");
 const router_1 = require("./routes/router");
 const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
 const server = new server_1.default();
 //BodyParser
 server.app.use(body_parser_1.default.urlencoded({ extended: true }));
 server.app.use(body_parser_1.default.json());
+//CORS
+server.app.use((0, cors_1.default)({ origin: true, credentials: true }));
+//Rutas de Servicio
 server.app.use('/', router_1.router);
 server.start(() => {
     console.log(`Servidor corriendo en el puerto ${environment_1.SERVER_PORT} `);
--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import Server from "./clases/server";
 import { SERVER_PORT } from "./global/environment";
 import {router} from './routes/router'
 import bodyParser  from "body-parser";
-
+import cors from 'cors';
 const  server=new Server();
 
 
@@ -11,7 +11,13 @@ const  server=new Server();
 
 server.app.use(bodyParser.urlencoded({extended:true}))
 server.app.use(bodyParser.json());
+//CORS
 
+server.app.use(cors({origin:true,credentials:true}));
+
+
+
+//Rutas de Servicio
 server.app.use('/',router);
 
 
