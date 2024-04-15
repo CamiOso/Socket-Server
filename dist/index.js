d70256030f0c1ec776074164309687f7eaea7527
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-04-15 01:43:02 -0500

    Se obtiene informacion del post y argumentos por URL

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./clases/server"));
 const environment_1 = require("./global/environment");
+const router_1 = require("./routes/router");
+const body_parser_1 = __importDefault(require("body-parser"));
 const server = new server_1.default();
+//BodyParser
+server.app.use(body_parser_1.default.urlencoded({ extended: true }));
+server.app.use(body_parser_1.default.json());
+server.app.use('/', router_1.router);
 server.start(() => {
     console.log(`Servidor corriendo en el puerto ${environment_1.SERVER_PORT} `);
 });
--- index.ts
+++ index.ts
@@ -1,7 +1,18 @@
 import Server from "./clases/server";
 import { SERVER_PORT } from "./global/environment";
+import {router} from './routes/router'
+import bodyParser  from "body-parser";
 
 const  server=new Server();
+
+
+
+//BodyParser
+
+server.app.use(bodyParser.urlencoded({extended:true}))
+server.app.use(bodyParser.json());
+
+server.app.use('/',router);
 
 
 server.start(()=>{
